dc86db9aa0da6d0f8c8cde442103286aa7a5d312
Nikita jha <86097451+Nikiurjew@users.noreply.github.com>
2024-08-02 19:03:44 +0530

    commit

--- src/components/Head/CTA.jsx
+++ src/components/Head/CTA.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import CV from '../../assets/CV.pdf'
+import React from 'react';
+import CV from '../../assets/CV.pdf';
+
+const downloadAndOpenPDF = () => {
+  const link = document.createElement('a');
+  link.href = CV;
+  link.download = 'CV.pdf';
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+
+  // Wait a bit to ensure the download has started
+  setTimeout(() => {
+    window.open(CV, '_blank');
+  }, 1000); // Adjust the delay as needed
+};
+
 const CTA = () => {
   return (
     <div className='cta'>
-    <a href={CV} download className='btn'>Download Resume</a>
-    <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+      <button className='btn btn-primary' onClick={downloadAndOpenPDF}>Download Resume</button>
+      <a href="#contact" className='btn btn-primary'>Let's Talk</a>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
